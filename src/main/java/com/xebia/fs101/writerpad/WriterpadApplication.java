6a1cc5ae1f4f67364d3643a0e43ebc424f93ea74
ankurrrsaxenaaa <ankurrrsaxenaaa@gmail.com>
2019-11-25 13:03:05 +0530

    checkstyle and spotbugs plugin added

--- src/main/java/com/xebia/fs101/writerpad/WriterpadApplication.java
+++ src/main/java/com/xebia/fs101/writerpad/WriterpadApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WriterpadApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WriterpadApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WriterpadApplication.class, args);
+    }
 }
